e2d11c49602cbbf9fb2abaeab7360f94239267d2
Sebastien Coppolani <sebcoppo@yahoo.fr>
2021-04-14 08:39:52 +0200

    First version

---
+++ src/components/app.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import Giflist from './gif_list';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xU9TT471DTGJq"
+    };
+  }
+
+  search = (query) => {
+    giphy('2e6BPq3THSggAXacZ3OGMJJAWYtBfzjW').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar search={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <Giflist gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
